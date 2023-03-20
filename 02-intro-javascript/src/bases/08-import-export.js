ec8995334afe907e9305612d750fc3168bdd4be3
Daniel Albanez <danielalbanez40@gmail.com>
2023-03-20 18:14:57 -0300

    promises

--- 02-intro-javascript/src/bases/08-import-export.js
+++ 02-intro-javascript/src/bases/08-import-export.js
@@ -14,9 +14,11 @@ import heroes, { owners } from '../data/heroes';
 
 const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
 const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
 
-console.log(getHeroesByOwner('Marvel'));
-console.log(owners);
+// console.log(getHeroesByOwner('Marvel'));
+// console.log(owners);
+
+export default getHeroeById
