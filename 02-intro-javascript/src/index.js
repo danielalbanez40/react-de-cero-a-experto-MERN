545828891fe29990772b83d1f2e50fe29d4d5b17
Daniel Albanez <danielalbanez40@gmail.com>
2023-03-27 18:00:53 -0300

    Operador Ternario

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,4 +1,18 @@
+// Operador ternario
 
+const activo = true;
+// let mensaje = '';
+
+// if (activo) {
+//     mensaje = 'Activo'
+// }else {
+//     mensaje = 'Inactivo'
+// }
+
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+// const mensaje = ( activo ) ? 'Activo' : null;
+
+const mensaje = activo && 'Activo'
 
 
 
