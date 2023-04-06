95be262b6c6d52a1322d90205979b9178bd283e9
Daniel Albanez <danielalbanez40@gmail.com>
2023-04-06 10:51:53 -0300

    print variables in HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const getResult = (a,b) => {
+  return a + b;
+}
+
 const FirstApp = () => {
+   
   return (
     <>
-      <h1>FirstApp</h1>
+      <h1>{getResult(4,4)}</h1>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
       <h2>Soy un subtítulo</h2>
     </>
   );
