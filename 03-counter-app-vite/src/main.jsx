bcb9c50be38952e9276ad3fbfc4a29a838429cc2
Daniel Albanez <danielalbanez40@gmail.com>
2023-04-06 23:45:58 -0300

    Tarea: CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HelloWorldApp from './HelloWorldApp'
-import FirstApp from './FirstApp';
+// import HelloWorldApp from './HelloWorldApp'
+// import FirstApp from './FirstApp';
 import './styles.css';
+import CounterApp from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorldApp />
-    <FirstApp title='Hola, sou Goku' subtitle='Soy un subtítulo'/>
+    {/* <HelloWorldApp />
+    <FirstApp title='Hola, sou Goku' subtitle='Soy un subtítulo'/> */}
+    <CounterApp value={10}/>
   </React.StrictMode>,
 );
