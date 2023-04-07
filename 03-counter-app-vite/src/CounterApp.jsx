e95243565133ccc13799653a1094f82509076e5a
Daniel Albanez <danielalbanez40@gmail.com>
2023-04-06 23:58:48 -0300

    Click event

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 const CounterApp = ({ value }) => {
+    
+    function handleAdd() {
+        // console.log(event);
+        console.log('+1');
+        
+    }
+    
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2> {value} </h2>
+      <button
+        onClick={handleAdd}
+      >
+        +1
+      </button>
     </>
   );
 };
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 
 export default CounterApp;
