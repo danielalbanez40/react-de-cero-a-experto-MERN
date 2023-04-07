425a9f0a85c536ebd69cfca0c9c77a01672c1d39
Daniel Albanez <danielalbanez40@gmail.com>
2023-04-06 23:29:11 -0300

    PropTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FirstApp = ({ title, subtitle }) => {
+const FirstApp = ({ title, subtitle='La tierra explota!', name }) => {
 
   return (
     <>
       <h1>{title}</h1>
       {/* <code>{JSON.stringify(newMessage)}</code> */}
       <p>{subtitle}</p>
+      <p>{name}</p>
     </>
   );
 };
@@ -15,6 +16,12 @@ const FirstApp = ({ title, subtitle }) => {
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay Título',
+  subtitle: 'No hay subtítulo',
+  name: 'Daniel Albanez'
 }
 
 export default FirstApp;
