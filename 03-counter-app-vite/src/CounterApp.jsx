19254f431a5b774b77ef738dcddd55f8d63587c7
Daniel Albanez <danielalbanez40@gmail.com>
2023-04-07 10:35:23 -0300

    handleSubstract & handleReset in counter with useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -2,19 +2,28 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
-  
   const [counter, setCounter] = useState(value);
-  
-  function handleAdd() {
+
+  const  handleAdd = () => {
     // setCounter((c) =>  c + 1);
     setCounter(counter + 1);
   }
 
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { counter } </h2>
-      <button onClick={handleAdd}>+1</button>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}> +1 </button>
+      <button onClick={handleSubstract}> -1 </button>
+      <button onClick={handleReset}> Reset </button>
     </>
   );
 };
