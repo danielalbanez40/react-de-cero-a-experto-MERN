9f9d076e2e4f10a0bd507c934d4aa6484caef5e8
Daniel Albanez <danielalbanez40@gmail.com>
2023-05-04 11:41:14 -0300

    Add Categories

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
 
 const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Punch','Dragon Ball Z']);
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball Z']);
+
+  const handleAddCategory = () => {
+
+    console.log(categories);
+    const addCategory = [...categories,'Valorant']
+    setCategories(addCategory);
+    // setCategories(cat => addCategory);
+  };
 
   return (
     <>
       {/* Titulo */}
       <h1>GifExpertApp</h1>
       {/* Input  */}
-      
+
       {/* Listado de Gifs */}
+      <button onClick={handleAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
