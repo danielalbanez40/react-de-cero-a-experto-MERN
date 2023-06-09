2bb49581b38fdeb0a5e2837bc24436c75318a2d2
Daniel Albanez <danielalbanez40@gmail.com>
2023-06-09 12:54:26 -0300

    GifExpertApp: Loading Message

--- 04-gif-expert-app/src/components/GifGrid/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid/GifGrid.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
-// import getGifs from "../../helpers/getGifs";
 import GifItem from "../GifItem/GifItem";
 import useFetchGifs from "../../hooks/useFetchGifs/useFetchGifs";
 
 const GifGrid = ({ category }) => {
   
-  const { images, isLoading } = useFetchGifs(category)
-   
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
 
+      {isLoading && <h2> Cargando... </h2>}
+
       <div className="card-grid">
         {images.map((image) => (
-          <GifItem key={image.id} {...image}/>
+          <GifItem key={image.id} {...image} />
         ))}
       </div>
     </>
