405ac802354d0f7cf7346332a8bbeb8966f6feb5
Daniel Albanez <danielalbanez40@gmail.com>
2023-06-27 18:39:24 -0300

    GifExpertApp: Simulate Form Submit

--- 04-gif-expert-app/src/components/AddCategory/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory/AddCategory.jsx
@@ -18,7 +18,7 @@ const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} aria-label="form">
       <input
         type="search"
         placeholder="Buscar Gifs"
--- 04-gif-expert-app/src/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.jsx
@@ -1,16 +1,29 @@
-import {fireEvent, render,screen} from '@testing-library/react'
-import AddCategory from '../../components/AddCategory/AddCategory';
+import { fireEvent, render, screen } from "@testing-library/react";
+import AddCategory from "../../components/AddCategory/AddCategory";
 
 describe("Test in <AddCategory></AddCategory>", () => {
   test("should change textbox value", () => {
-
-    render(<AddCategory onNewCategory={() => {}}/>);
-    const input = screen.getByRole('searchbox'); //searchbox, textbox, etc, depende del valor del type del input html
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole("searchbox"); //searchbox, textbox, etc, depende del valor del type del input html
 
     // Disparo el evento
-    fireEvent.input(input, {target: {value: 'Saitama'}});
-    expect(input.value).toBe('Saitama')
+    fireEvent.input(input, { target: { value: "Saitama" } });
+    expect(input.value).toBe("Saitama");
+    // screen.debug()
+  });
+
+  test("should call to handleSubmit if the input has a value", () => {
+    const inputValue = "Saitama";
+    // TODO: ???
+    render(<AddCategory onNewCategory={() => {}} />);
+
+    const input = screen.getByRole("searchbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
     // screen.debug()
 
+    expect(input.value).toBe("");
   });
 });
